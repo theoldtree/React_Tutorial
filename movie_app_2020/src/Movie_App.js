98f9e62035226f6cc529ab4a68bedefdfc1d94ed
유정무 <yujeongmu@MacBook-Pro.local>
2020-08-30 21:26:43 +0900

    [#8] feat : index.js파일에 Movie_App 컴포넌트 추가, 삼항연사자는 아직 구현하지 않음

---
+++ movie_app_2020/src/Movie_App.js
@@ -0,0 +1,9 @@
+import React, {useState} from 'react';
+
+export default function Movie_App(){
+  return(
+    <div>
+      <h3>Movie_App</h3>
+    </div>
+  );
+}
--- movie_app_2020/src/index.js
+++ movie_app_2020/src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Movie_App from './Movie_App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Movie_App />
   </React.StrictMode>,
   document.getElementById('root')
 );
