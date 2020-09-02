a92d3f831eb79ab22d7fae1dc0114e8a6b3762a9
유정무 <yujeongmu@MacBook-Pro.local>
2020-09-02 16:52:33 +0900

    Updates

---
+++ precache-manifest.4667f74609ebea350a0aac67648dc509.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80bec4a90b7d6f5d05f6e3bbe63068cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2271c7f1b0992cd5e3d4",
+    "url": "/static/css/main.c698557a.chunk.css"
+  },
+  {
+    "revision": "d9953d6af62d4fefb954",
+    "url": "/static/js/2.95ae9877.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.95ae9877.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2271c7f1b0992cd5e3d4",
+    "url": "/static/js/main.69842d5a.chunk.js"
+  },
+  {
+    "revision": "bd7f4f55b55e38cea2f1",
+    "url": "/static/js/runtime-main.58002af7.js"
+  }
+]);
